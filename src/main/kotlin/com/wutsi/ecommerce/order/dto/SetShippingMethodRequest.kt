0e34ccea502bdc0881dd93ee2ec5ae7d72eb7ef8
wutsi <herve.tchepannou@wutsi.com>
2022-03-03 08:49:39 -0500

    update api

--- src/main/kotlin/com/wutsi/ecommerce/order/dto/SetShippingMethodRequest.kt
+++ src/main/kotlin/com/wutsi/ecommerce/order/dto/SetShippingMethodRequest.kt
@@ -1,9 +1,10 @@
 package com.wutsi.ecommerce.order.dto
 
-import kotlin.Double
 import kotlin.Long
+import kotlin.String
 
 public data class SetShippingMethodRequest(
   public val shippingId: Long = 0,
-  public val shippingRate: Double = 0.0
+  public val country: String = "",
+  public val cityId: Long? = null
 )
