8d14da6548bc8b0e055539dd1479c88fe00ed2cf
wutsi <herve.tchepannou@wutsi.com>
2022-03-03 13:04:30 -0500

    update schema

--- src/main/kotlin/com/wutsi/ecommerce/order/dto/SetShippingMethodRequest.kt
+++ src/main/kotlin/com/wutsi/ecommerce/order/dto/SetShippingMethodRequest.kt
@@ -1,10 +1,7 @@
 package com.wutsi.ecommerce.order.dto
 
 import kotlin.Long
-import kotlin.String
 
 public data class SetShippingMethodRequest(
-  public val shippingId: Long = 0,
-  public val country: String = "",
-  public val cityId: Long? = null
+  public val shippingId: Long = 0
 )
--- src/main/kotlin/com/wutsi/ecommerce/order/error/ErrorURN.kt
+++ src/main/kotlin/com/wutsi/ecommerce/order/error/ErrorURN.kt
@@ -1,7 +1,11 @@
 package com.wutsi.ecommerce.order.error
 
 enum class ErrorURN(val urn: String) {
+    ADDRESS_NOT_FOUND("urn:wutsi:error:order:address-not-found"),
     ORDER_NOT_FOUND("urn:wutsi:error:order:order-not-found"),
+    SHIPPING_NOT_FOUND("urn:wutsi:error:order:shipping-not-found"),
+    PRODUCT_NOT_FOUND("urn:wutsi:error:order:product-not-found"),
+    ILLEGAL_STATUS("urn:wutsi:error:order:illegal-status"),
     INVALID_PRODUCT_ID("urn:wutsi:error:order:invalid-product-id"),
     PRODUCT_AVAILABILITY_ERROR("urn:wutsi:error:order:product-availability-error"),
     RESERVATION_ERROR("urn:wutsi:error:order:reservation-error"),
